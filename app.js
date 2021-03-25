0e0ae26a2550e32224866d441c91d0491255dcf4
Miral-Alabdullah <miral.alabdallah@gmail.com>
2021-03-25 15:19:53 +0200

    Adding Javascript

---
+++ app.js
@@ -0,0 +1,19 @@
+
+    var userName = prompt("Enter your username:", "Hint: Miral");
+    var password = prompt("Enter your password", "Hint: 0000");
+    
+
+    while (userName != "Miral" && password != "0000")
+    {
+        userName = prompt("Enter your username:", "Hint: Miral");
+        password = prompt("Enter your password", "Hint: 0000");
+    }
+
+    function showAlert()
+    {
+        alert("This page is not available yet")
+    } 
+
+    
+    
+    
