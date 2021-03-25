5fca1895212cbae260c0292ac44d13fe66943c93
Miral-Alabdullah <miral.alabdallah@gmail.com>
2021-03-25 15:39:09 +0200

    Add a document

--- app.js
+++ app.js
@@ -7,7 +7,10 @@
     {
         userName = prompt("Enter your username:", "Hint: Miral");
         password = prompt("Enter your password", "Hint: 0000");
+        
     }
+    document.write("<h3> Welcome to our website</h3>")
+    
 
     function showAlert()
     {
