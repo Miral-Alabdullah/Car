77e0786573d6aaa0981c8b2886997878b065223c
Miral-Alabdullah <miral.alabdallah@gmail.com>
2021-03-25 15:43:02 +0200

    adding another alert

--- app.js
+++ app.js
@@ -5,17 +5,19 @@
 
     while (userName != "Miral" && password != "0000")
     {
-        userName = prompt("Enter your username:", "Hint: Miral");
-        password = prompt("Enter your password", "Hint: 0000");
+        userName = prompt("Enter your username:", "Hint: Miral, Try it wrongly!");
+        password = prompt("Enter your password", "Hint: 0000, Try it wrongly!");
         
     }
-    document.write("<h3> Welcome to our website</h3>")
+     alert(" Welcome to our website " + userName)
     
 
     function showAlert()
     {
         alert("This page is not available yet")
     } 
+
+    
 
     
     
