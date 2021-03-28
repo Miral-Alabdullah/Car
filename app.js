d4d3153f785423b1a9a33bd3c168be28be194c63
Miral-Alabdullah <miral.alabdallah@gmail.com>
2021-03-28 14:38:56 +0300

    Adding loops

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
 
-    var userName = prompt("Enter your username:", "Hint: Miral");
-    var password = prompt("Enter your password", "Hint: 0000");
+    var userName = prompt("Enter your username:", "Hint: Miral, Try it wrongly!");
+    var password = prompt("Enter your password", "Hint: 0000, Try it wrongly!");
     
 
     while (userName != "Miral" && password != "0000")
     {
-        userName = prompt("Enter your username:", "Hint: Miral, Try it wrongly!");
-        password = prompt("Enter your password", "Hint: 0000, Try it wrongly!");
+        userName = prompt("Enter your username again:", "Hint: Miral");
+        password = prompt("Enter your password again", "Hint: 0000");
         
     }
      alert(" Welcome to our website " + userName)
+
+
+
+
+    var image = '';
+    var carName = prompt("Choose one of these cars:","BMW, Mercedes, Range Rover");
+    var carNumber = prompt("How many times you want this car?");
+    for (var i=0;i<=carNumber;i++)
+        {
+            if(carName === "BMW")
+            {
+                image = image + '<img src="https://www.ccarprice.com/products/BMW-8-Series-840i-xDrive-Coupe-2020_1.jpg"/>';
+            } else if (carName === "Mercedes") 
+            {
+                image = image + '<img src="https://cars.usnews.com/static/images/Auto/izmo/i159580569/2020_mercedes_benz_cla_angularfront.jpg"/>';                
+            } else if (carName === "Range Rover")
+            {
+                 image = image + '<img src="https://i.pinimg.com/originals/26/e6/7f/26e67f0e92d14a8336f825eb5cc2e9a1.jpg"/>';
+            } 
+        }
+        console.log(image);
+        document.write(image);
+            
+
+        
+
+    
+
     
 
     function showAlert()
@@ -17,8 +45,9 @@
         alert("This page is not available yet")
     } 
 
-    
 
-    
+
+
+
     
     
